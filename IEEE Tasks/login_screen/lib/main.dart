60589777145e6a4a357d5e45318dc4932df7a43c
AhmedSarg <xsarg22@gmail.com>
2023-02-03 01:32:39 +0200

    new

--- IEEE Tasks/login_screen/lib/main.dart
+++ IEEE Tasks/login_screen/lib/main.dart
@@ -33,82 +33,35 @@ class NewApp extends StatefulWidget {
 }
 
 class _NewAppState extends State<NewApp> {
-  MaterialColor clr = Colors.green;
-  String clrTitle = "Green";
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text(clrTitle),
-          backgroundColor: clr,
+          title: const Text("StateFull"),
+          backgroundColor: Colors.green,
         ),
         body: Container(
           width: double.infinity,
           height: double.infinity,
           color: Colors.white,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                margin: const EdgeInsets.all(20),
-                child: TextButton(
-                    onPressed: () {
-                      setState(() {
-                        clr = Colors.red;
-                        clrTitle = "Red";
-                      });
-                    },
-                    child: Container(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 10, horizontal: 20),
-                        color: Colors.red,
-                        child: const Text(
-                          "Red",
-                          style: TextStyle(color: Colors.white, fontSize: 13.0),
-                        ))),
-              ),
-              Container(
-                margin: const EdgeInsets.all(20),
-                child: TextButton(
-                    onPressed: () {
-                      setState(() {
-                        clr = Colors.green;
-                        clrTitle = "Green";
-                      });
-                    },
-                    child: Container(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 10, horizontal: 20),
-                        color: Colors.green,
-                        child: const Text(
-                          "Greennnnnnn",
-                          style: TextStyle(color: Colors.white, fontSize: 13.0),
-                        ))),
-              ),
-              Container(
-                margin: const EdgeInsets.all(20),
-                child: TextButton(
-                    onPressed: () {
-                      setState(() {
-                        clr = Colors.blue;
-                        clrTitle = "Blue";
-                      });
-                    },
-                    child: Container(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 10, horizontal: 20),
-                        color: Colors.blue,
-                        child: const Text(
-                          "Blue",
-                          style: TextStyle(color: Colors.white, fontSize: 13.0),
-                        ))),
-              ),
-            ],
+          child: Center(
+            child: TextButton(
+              onPressed: () {},
+              child: Container(
+                padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                color: Colors.green,
+                child: const Text(
+                    "Ahmed",
+                  style: TextStyle(color: Colors.white, fontSize: 13.0),
+                )
+              )
+            ),
           ),
         ),
       ),
     );
   }
 }
+
