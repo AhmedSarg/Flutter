27328ca6b9c27554ab679f4e12a737fbda568da8
AhmedSarg <xsarg22@gmail.com>
2023-02-03 14:02:30 +0200

    new

--- IEEE Tasks/login_screen/lib/main.dart
+++ IEEE Tasks/login_screen/lib/main.dart
@@ -36,26 +36,182 @@ class _NewAppState extends State<NewApp> {
   MaterialColor _clr = Colors.green;
   String _clrTitle = "Green";
   bool _passwordVisible = false;
-
+  String _email = "", _password = "";
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: Text(_clrTitle),
-          backgroundColor: _clr,
-        ),
         body: Container(
           width: double.infinity,
           height: double.infinity,
-          color: Colors.white,
+          color: Colors.deepPurple,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Column(
-                children: [
-                  TextFormField(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  RotationTransition(
+                    turns: AlwaysStoppedAnimation(-40/360),
+                    child: Text(
+                        "E",
+                      style: TextStyle(
+                        color: Colors.orange,
+                        fontWeight: FontWeight.w800,
+                        fontSize: 125,
+                      ),
+                    ),
+                  ),
+                  Text(
+                      "  ECORP",
+                    style: TextStyle(
+                      color: Colors.orange,
+                      fontSize: 50
+                    ),
+                  )
+                ],
+              ),
+              Container(
+                width: 300,
+                height: 300,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20)
+                ),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        prefixIcon: Icon(
+                          Icons.account_circle,
+                          color: Colors.grey,
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              width: 3,
+                              color: Colors.white,
+                              style: BorderStyle.solid
+                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              width: 3,
+                              color: Colors.white,
+                              style: BorderStyle.solid
+                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
+                        ),
+                        hintText: "Email",
+                        hintStyle: TextStyle(color: Colors.grey),
+                        filled: true,
+                        fillColor: Colors.black12,
+                      ),
+                      validator: (String? value) {
+                        if (value != null && !value.contains('@')) {
+                          return "Type Correct Email";
+                        }
+                        return null;
+                      },
+                      onSaved: (String? value) {
+                        _email = value!;
+                      },
+                      obscureText: false,
+                      style: const TextStyle(color: Colors.black),
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        prefixIcon: const Icon(
+                          Icons.lock,
+                          color: Colors.grey,
+                        ),
+                        suffixIcon: IconButton(
+                            onPressed: () {
+                              setState(() {
+                                _passwordVisible = !_passwordVisible;
+                              });
+                            },
+                            icon: Icon(
+                              _passwordVisible? Icons.visibility : Icons.visibility_off,
+                              color: Colors.grey,
+                            )
+                        ),
+                        enabledBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(
+                              width: 3,
+                              color: Colors.white,
+                              style: BorderStyle.solid
+                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
+                        ),
+                        focusedBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(
+                              width: 3,
+                              color: Colors.white,
+                              style: BorderStyle.solid
+                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
+                        ),
+                        hintText: "Password",
+                        hintStyle: const TextStyle(color: Colors.grey),
+                        filled: true,
+                        fillColor: Colors.black12,
+                      ),
+                      validator: (String? value) {
+                        if (value != null && value.length < 8) {
+                          return "Type Correct Password";
+                        }
+                        return null;
+                      },
+                      onSaved: (String? value) {
+                        _password = value!;
+                      },
+                      obscureText: !_passwordVisible,
+                      style: const TextStyle(color: Colors.black),
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                    TextButton(
+                        onPressed: () {},
+                        child: const Text(
+                            "Forgot Password?",
+                          style: TextStyle(color: Colors.grey),
+                        )
+                    ),
+                    Column(
+                      children: [
+                        TextButton(
+                          onPressed: () {},
+                          child: Container(
+                            width: 100,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50),
+                              color: Colors.deepPurple
+                            ),
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 20),
+                            child: const Center(
+                              child: Text(
+                                "Login",
+                                style: TextStyle(color: Colors.white, fontSize: 13.0),
+                              ),
+                            )
+                          )
+                        ),
+                      ],
+                    )
+                  ]
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+/*TextFormField(
                     decoration: InputDecoration(
                       prefixIcon: Icon(
                           Icons.email,
@@ -101,15 +257,9 @@ class _NewAppState extends State<NewApp> {
                     },
                     obscureText: !_passwordVisible,
                     style: TextStyle(color: _clr),
-                  )
-                ],
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Container(
-                    margin: const EdgeInsets.all(20),
-                    child: TextButton(
+                  )*/
+
+/*TextButton(
                         onPressed: () {
                           setState(() {
                             _clr = Colors.red;
@@ -123,50 +273,7 @@ class _NewAppState extends State<NewApp> {
                             child: const Text(
                               "Red",
                               style: TextStyle(color: Colors.white, fontSize: 13.0),
-                            ))),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.all(20),
-                    child: TextButton(
-                        onPressed: () {
-                          setState(() {
-                            _clr = Colors.green;
-                            _clrTitle = "Green";
-                          });
-                        },
-                        child: Container(
-                            padding: const EdgeInsets.symmetric(
-                                vertical: 10, horizontal: 20),
-                            color: Colors.green,
-                            child: const Text(
-                              "Green",
-                              style: TextStyle(color: Colors.white, fontSize: 13.0),
-                            ))),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.all(20),
-                    child: TextButton(
-                        onPressed: () {
-                          setState(() {
-                            _clr = Colors.blue;
-                            _clrTitle = "Blue";
-                          });
-                        },
-                        child: Container(
-                            padding: const EdgeInsets.symmetric(
-                                vertical: 10, horizontal: 20),
-                            color: Colors.blue,
-                            child: const Text(
-                              "Blue",
-                              style: TextStyle(color: Colors.white, fontSize: 13.0),
-                            ))),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+                            )
+                        )
+                    ),
+                  ),*/
